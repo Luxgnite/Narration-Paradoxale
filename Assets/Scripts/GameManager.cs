a87aecd3196a47746e2cf4d7e2b244379d51b0df
Luxgnite <ewan.dubois@live.fr>
2020-02-06 03:00:45 +0100

    Base Interactible (click)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Camera camera;
+
+    private void Awake()
+    {
+        camera = GameObject.FindObjectOfType<Camera>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
